a8c7a5ce87711f3f68f3fae01e81f3586425f5b6
aruizga7 <armand.ruiz@gmail.com>
2015-04-12 22:48:48 -0500

    yes

--- js/projects.js
+++ js/projects.js
@@ -5,27 +5,27 @@ oss_projects['TitanicShinyApplication'] = {
   position: 3,
   background: 'dockersh_bg.png'
 }
-oss_projects['MOE'] = {
+oss_projects['TitanicShinyApplication'] = {
   blog_post: 'http://engineeringblog.yelp.com/2014/07/introducing-moe-metric-optimization-engine-a-new-open-source-machine-learning-service-for-optimal-ex.html',
   featured: true,
   position: 1,
   logo: 'moe_logo.png',
   background: 'moe_bg.png'
 };
-oss_projects['mrjob'] = {
+oss_projects['TitanicShinyApplication'] = {
   blog_post: 'http://engineeringblog.yelp.com/2010/10/mrjob-distributed-computing-for-everybody.html',
   featured: true,
   position: 2,
   logo: 'mrjob_logo.png',
   background: 'mrjob_bg.png'
 };
-oss_projects['yelp-ruby'] = {
+oss_projects['TitanicShinyApplication'] = {
   blog_post: 'http://engineeringblog.yelp.com/2014/04/more-yelp-in-your-ruby.html'
 };
-oss_projects['firefly'] = {
+oss_projects['TitanicShinyApplication'] = {
   blog_post: 'http://engineeringblog.yelp.com/2012/08/firefly-illuminate-your-websites-performance.html',
 };
-oss_projects['pyleus'] = {
+oss_projects['TitanicShinyApplication'] = {
   blog_post: 'http://engineeringblog.yelp.com/2014/10/introducing-pyleus.html',
 };
 
